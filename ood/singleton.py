66fa122290aede8dd8e0a8dea36c5a4e36f29329
haoqianglyu <haoqianglyu@gmail.com>
2021-02-04 09:49:51 +0800

    add singleton.py

---
+++ ood/singleton.py
@@ -0,0 +1,23 @@
+# 单例设计模式，某个类整个程序运行期间最多只能有一个对象被创建
+
+class User(object):
+    # 私有属性
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        # if not cls.__instance:
+        #     return super().__new__(cls)
+        # else:
+        #     return  cls.__instance
+        if not cls.__instance:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    # def __init__(self,name):
+    #     pass
+
+
+user1 = User("william")
+user2 = User("leo")
+print(id(user1))
+print(id(user2))
